fbfbccf25db371d724783314502ed897b76ffda3
sktrombone <x.file.tip@gmail.com>
2017-11-29 22:52:29 +0900

    Add files via upload

--- mp1/cosk1.py
+++ mp1/cosk1.py
@@ -1,9 +1,11 @@
 import requests
 from requests.auth import HTTPDigestAuth
-url = 'http://kosaka004.je.tokyo-ct.ac.jp/studentreports/3Jucom.php?'
 set = requests.Session()
+url = 'http://kosaka004.je.tokyo-ct.ac.jp/studentreports/3Jucom.php?'
 r = set.get(url, auth=HTTPDigestAuth('s15097', 's15097'))
 files = {'fileselect':('mp5ex01.txt', open('mp5ex01.txt','rb'),'text/plain')}
 res = set.post(url,files=files)
-res.text
+print (res.text)
 print (res.status_code , '\n' , res.headers)
+f = open("cosk1.html","w")
+f.write(res.text)
