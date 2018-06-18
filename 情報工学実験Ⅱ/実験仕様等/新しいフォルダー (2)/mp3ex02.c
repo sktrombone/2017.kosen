376eb419ff61a44bafea231da3cdbf1e85b66196
sktrombone <x.file.tip@gmail.com>
2018-06-18 09:46:57 +0900

    new

---
+++ 情報工学実験Ⅱ/実験仕様等/新しいフォルダー (2)/mp3ex02.c
@@ -0,0 +1,27 @@
+/**********************************************************
+msecwaitŠÖ”‚Å1•b‚²‚Æ‚ÌLED‚ÌON-OFF‚ğs‚¤
+**********************************************************/
+#include <3048fone.h>
+#include "h8_3048fone.h"
+
+void waitmsec(int msec)
+{
+	int i,j;
+	for (i=0;i<msec;i++) {
+		for (j=0;j<4190;j++);    /*4190‚ÍÀ‘ª‚É‚æ‚Á‚Ä‹‚ß‚½’l 25MHz‹ì“®*/
+	}
+}
+
+main()
+{
+	initLed();
+	while(1) {
+		turnOnLed(0); /*LED0‚ÌON*/
+		turnOnLed(1); /*LED1‚ÌON*/
+		waitmsec(250);
+		
+		turnOffLed(0); /*LED0‚ÌOFF*/
+		turnOffLed(1); /*LED1‚ÌOFF*/
+		waitmsec(250);
+	}
+}
---
+++ 情報工学実験Ⅱ/実験仕様等/新しいフォルダー (2)/mp3ex03.c
@@ -0,0 +1,49 @@
+/**********************************************************
+msecwaitŠÖ”‚Å1•b‚²‚Æ‚ÌLED‚ÌON-OFF‚ğs‚¤
+**********************************************************/
+#include <3048fone.h>
+#include "h8_3048fone.h"
+
+void waitmsec(int msec)
+{
+	int i,j;
+	for (i=0;i<msec;i++) {
+		for (j=0;j<4190;j++);    /*4190‚ÍÀ‘ª‚É‚æ‚Á‚Ä‹‚ß‚½’l 25MHz‹ì“®*/
+	}
+}
+
+main()
+{
+	initLed();
+	while(1) {
+		turnOnLed(1); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOffLed(1); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOnLed(1); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOffLed(1); /*LED0‚ÌON*/
+		waitmsec(500);
+		
+		turnOnLed(0); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOffLed(0); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOnLed(0); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOffLed(0); /*LED0‚ÌON*/
+		waitmsec(500);
+		
+		turnOnLed(1); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOffLed(1); /*LED0‚ÌON*/
+		waitmsec(500);
+		
+		turnOnLed(0); /*LED0‚ÌON*/
+		waitmsec(500);
+		turnOffLed(0); /*LED0‚ÌON*/
+		waitmsec(500);
+
+				
+	}
+}
